9e71c626f22661b8a39a1cd71d7a5b9318d01bd0
syiroth <irothsyiroth@gmail.com>
2026-03-03 15:19:54 +0700

    update

--- app/Filament/Resources/Items/Tables/ItemsTable.php
+++ app/Filament/Resources/Items/Tables/ItemsTable.php
@@ -8,7 +8,6 @@ use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Actions\ViewAction;
-use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
