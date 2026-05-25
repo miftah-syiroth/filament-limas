6dc6c6fc9085068eccfbf0b7d71e317f27b97ae2
syiroth <irothsyiroth@gmail.com>
2026-05-25 13:09:18 +0700

    Refactor QrCodeEntry to use DNS1DFacade for barcode generation, switching from QR code to Code 128 format for improved compatibility.

--- app/Filament/Infolists/Components/QrCodeEntry.php
+++ app/Filament/Infolists/Components/QrCodeEntry.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Infolists\Components;
 
 use Filament\Infolists\Components\Entry;
+use Milon\Barcode\Facades\DNS1DFacade;
 use Milon\Barcode\Facades\DNS2DFacade;
 
 class QrCodeEntry extends Entry
@@ -14,7 +15,8 @@ class QrCodeEntry extends Entry
         $value = (string) $this->getState();
 
         return $value !== ''
-            ? 'data:image/png;base64,'.DNS2DFacade::getBarcodePNG($value, 'QRCODE', 10, 10)
+            // ? 'data:image/png;base64,'.DNS2DFacade::getBarcodePNG($value, 'QRCODE', 10, 10)
+            ? 'data:image/png;base64,'.DNS1DFacade::getBarcodePNG($value, 'C128', 1, 50)
             : '';
     }
 }
