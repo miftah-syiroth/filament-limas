59d6cf510304ceaa329622aabaa8aa1c237196d4
syiroth <irothsyiroth@gmail.com>
2026-06-22 14:43:05 +0700

    update

--- config/fortify.php
+++ config/fortify.php
@@ -147,7 +147,7 @@ return [
     'features' => [
         // Features::registration(),
         // Features::resetPasswords(),
-        Features::emailVerification(),
+        // Features::emailVerification(),
         Features::twoFactorAuthentication([
             'confirm' => true,
             'confirmPassword' => true,
